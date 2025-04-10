07c4aeb33dbdafba37dd61447700f2bf32ae27eb
Ekaterina Mikaylova <ekaterinamikaylova@Ekaterinas-Air.home>
2025-04-10 23:28:30 +0200

    converter to int **map added

--- main.c
+++ main.c
@@ -13,6 +13,62 @@
 //#include "game.h"
 #include "parser.h"
 
+
+int	**convert_char_map_to_int(t_data data)
+{
+	int		**int_map;
+	size_t	i, j;
+
+	int_map = malloc(sizeof(int *) * data.rows);
+	if (!int_map)
+		return (NULL);
+	i = 0;
+	while (i < data.rows)
+	{
+		int_map[i] = malloc(sizeof(int) * data.cols);
+		if (!int_map[i])
+		{
+			// Free previously allocated rows on error
+			while (i > 0)
+				free(int_map[--i]);
+			free(int_map);
+			return (NULL);
+		}
+		j = 0;
+		while (j < data.cols)
+		{
+			// Convert char to int (example: '1' -> 1, '0' -> 0, etc.)
+			int_map[i][j] = data.map[i][j] - '0';
+			if(data.map[i][j] == 'N' || data.map[i][j] == 'S'
+				|| data.map[i][j] == 'E' || data.map[i][j] == 'W')
+				int_map[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	return (int_map);
+}
+
+void	print_int_map(int **map, size_t rows, size_t cols)
+{
+	size_t i, j;
+
+	i = 0;
+	while (i < rows)
+	{
+		j = 0;
+		while (j < cols)
+		{
+			printf("%d", map[i][j]);
+			if (j < cols - 1)
+				printf(" ");
+			j++;
+		}
+		printf("\n");
+		i++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	//t_game	game_data;
@@ -35,5 +91,7 @@ int	main(int argc, char **argv)
 	printf("Floor color %d, celiling color %d\n", data.floor, data.ceiling);
 	printf("Map size rows %zu, cols %zu\n", data.rows, data.cols);
 	printf("Position direction %c, coord [%d].[%d]\n", data.me.dir, data.me.pos.x, data.me.pos.y);
+	int **map = convert_char_map_to_int(data);
+	print_int_map(map, data.rows, data.cols); //check
 	return (0);
 }
