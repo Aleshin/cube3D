f15d5cf737a669595902d5e0d76c1d7b579d668a
Sergey Aleshin <saleshin@cbr7s2.42barcelona.com>
2025-04-18 23:35:03 +0200

    fix dev by zero segfault in calculate_projection

--- src/init.c
+++ src/init.c
@@ -36,12 +36,10 @@ void	init_player(t_game *data)
 	data->dir_y = 0;
 	data->plane_x = 0;
 	data->plane_y = FOV;
-
 	data->textures.north.path = NULL;
 	data->textures.south.path = NULL;
 	data->textures.east.path = NULL;
 	data->textures.west.path = NULL;
-	
 	gettimeofday(&data->prev_time, NULL);
 }
 
@@ -56,19 +54,14 @@ void	init_window(t_game *data)
 
 void	init_map(t_game *data, t_data input)
 {
-	
 	data->width = (int)input.cols;
 	data->height = (int)input.rows;
-
-	//input->map
 	data->map = convert_char_map_to_int(input);
 	if (!data->map)
 	{
 		printf("Error: failed to convert map\n");
 		exit(0);
-	// Handle the error here (e.g., free memory or exit)
 	}
-
 	data->textures.north.path = ft_strdup(input.no);
 	data->textures.south.path = ft_strdup(input.so);
 	data->textures.east.path = ft_strdup(input.ea);
